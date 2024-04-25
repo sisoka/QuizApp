565897bff7b34f40153caa983c9b5ed55454cea2
sisko <sisoka1988@abv.bg>
2024-04-25 23:42:43 +0300

    functionality added for creating a api url and fetch initial data from the api. Some small changes in the html and css

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 function loadQuizPage(){
         window.location.href = "quizPage.html";
-}    
-
-const dom ={
-    startBtn : document.querySelector('.btnStart')
+        let apiUrl = generateApiUrl();
+        console.log(apiUrl);
+        getQuestionsInitially(apiUrl);
 }
 
+function generateApiUrl(){
+    let amount = dom.selectQuestionsAmount.value;
+    let category = dom.selectCategory.value;
+    let difficulty = dom.selectDifficulty.value;
+    return `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`;
+}
+
+async function getQuestionsInitially(apiUrl) {
+    const response = await fetch(apiUrl);
+    const qustions = await response.json();
+    console.log(qustions);
+}
+
+
+const dom ={
+    startBtn : document.querySelector('.btnStart'),
+    selectCategory: document.querySelector('#triviaCategory'),
+    selectDifficulty: document.querySelector('#triviaDifficulty'),
+    selectQuestionsAmount: document.querySelector('#triviaQuestionAmount')
+}
+
+let qustions = [];
+
 dom.startBtn.addEventListener('click',loadQuizPage);
+
+
+
 
